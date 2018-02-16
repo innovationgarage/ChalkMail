335bf6eaa6918e8251b9348108b31a9870dc895b
asadisaghar <asadisaghar@gmail.com>
2018-02-16 17:34:42 +0100

    now can add multiple drawing on one background, augmentation is still limited to 90degree rotation

--- email_detector/envelope/add_template_to_backgrounds.py
+++ email_detector/envelope/add_template_to_backgrounds.py
@@ -8,42 +8,53 @@ import os
 def read_img(img_path, img_name):
     return Image.open(os.path.join(img_path, img_name)).convert("L")
     
-def draw_strokes(strokes, img):
+def draw_strokes(strokes, img, x_offset=0, y_offset=0):
     draw = ImageDraw.Draw(img)
     for stroke in strokes:
         row = stroke[0]
         col = stroke[1]
         for i in range(len(row)-1):
-            draw.line((row[i], col[i], row[i+1], col[i+1]), fill=50, width=10)
+            draw.line((row[i]+x_offset, col[i]+y_offset, row[i+1]+x_offset, col[i+1]+y_offset), fill=50, width=2)
     return img
 
 def get_strokes(drawing):
-    rows = [drawing[i][0] for i in range(len(drawing))]
-    cols = [drawing[i][1] for i in range(len(drawing))]
-    w_s = np.max(rows) - np.min(rows)
-    h_s = np.max(cols) - np.min(cols)
-    strokes = [(r, c) for (r,c) in zip(rows, cols)]
+    rows = np.array([drawing[i][0] for i in range(len(drawing))])
+    cols = np.array([drawing[i][1] for i in range(len(drawing))])
+    w_s = np.max(np.max(rows)) - np.min(np.min(rows))
+    h_s = np.max(np.max(cols)) - np.min(np.min(cols))
+    if np.random.random()>=0.5:
+        strokes = [(r, c) for (r,c) in zip(rows, cols)]
+    else:
+        strokes = [(r, c) for (r,c) in zip(cols, rows)]
     return strokes, w_s, h_s
     
-def draw_on_background(bg_path, bg_name):
-    bg = read_img(bg_path, bg_name)
-    w_bg, h_bg = bg.size
-    img_path = "envelope/bg_images"
+def draw_on_background(bg_path, bg_name, df, out_path):
     drawing_type = 'envelope'
-    df = pd.read_json('envelope/full_raw_envelope.ndjson', lines=True)
-    for i, drawing in enumerate(df.drawing.values[:10]):
+    samples = df.sample(30)
+    for i, drawing in enumerate(samples.drawing.values):
+        bg = read_img(bg_path, bg_name)
+        w_bg, h_bg = bg.size
         label = '%s.%d'%(drawing_type, i)
-        strokes, w_s, h_s = get_strokes(drawing)
-        strokes = np.array(strokes)
-        scale_h = 15./(h_bg*1./h_s*1.)
-        scale_w = 15./(w_bg*1./w_s*1.)
-        scale_factor = np.array([scale_h, scale_w]).mean()
-        strokes = np.array(strokes)/scale_factor
-        img = draw_strokes(strokes, bg)
-        img.save(os.path.join(img_path, "%s.jpg"%label), quality=100)
-
-
-bg_path = 'envelope/backgrounds/'
-bg_name = 'wb.21.jpg'
-img_path = 'envelope/res/'
-draw_on_background(bg_path, bg_name)
+        try:
+            strokes, w_s, h_s = get_strokes(drawing)
+            strokes = np.array(strokes)
+            scale_h = 20./(h_bg*1./h_s*1.)
+            scale_w = 20./(w_bg*1./w_s*1.)
+            scale_factor = np.array([scale_h, scale_w]).mean()
+            strokes = np.array(strokes)/scale_factor
+            # x_offset = (np.random.random() * w_bg) - w_s*scale_factor
+            # y_offset = (np.random.random() * h_bg) - h_s*scale_factor
+            x_offset = 0.8 * w_bg
+            y_offset = 0.1 * h_bg
+            img = draw_strokes(strokes, bg, x_offset, y_offset)
+            print os.path.join(out_path, "%s.jpg"%label)
+            img.save(os.path.join(out_path, "%s_%s_08_01.jpg"%(label, bg_name.split('.jpg')[0])), quality=100)
+        except:
+            print 'drawing %s failed!'%label
+            
+bg_path = 'envelope/tmp1/'
+#bg_name = 'wb.2.jpg'
+img_path = 'envelope/tmp/'
+df = pd.read_json('envelope/full_raw_envelope.ndjson', lines=True)
+for bg_name in os.listdir(bg_path):
+    draw_on_background(bg_path, bg_name, df, img_path)
